f85b775a3a31a8c46f6b731ac76500d769337991
Naib <naibmagamadov0@gmail.com>
2026-08-26 14:23:06 +0200

    evcevev

--- CPP_module_09/ex02/PmergeMe.cpp
+++ CPP_module_09/ex02/PmergeMe.cpp
@@ -1,1 +1,26 @@
 #include "PmergeMe.hpp"
+
+void PmergeMe::oddChecker()
+{
+    if (_vec.size() % 2)
+    {
+        _odd = _vec.back();
+        _vec.pop_back();
+    }
+}
+
+void PmergeMe::convert_vec(const char **input)
+{
+    for (int i = 0; input[i]; i++)
+        _vec.push_back(std::strtol(input[i], NULL, 10));
+    oddChecker();
+}
+
+void PmergeMe::sort()
+{
+    std::vector< std::pair<int, int> > pairVec;
+    for (int i = 0; i < _vec.size(); i += 2)
+        pairVec.push_back(std::make_pair(_vec[i], _vec[i + 1]));
+    std::vector<int> tmp;
+    
+}
--- CPP_module_09/ex02/PmergeMe.hpp
+++ CPP_module_09/ex02/PmergeMe.hpp
@@ -8,6 +8,29 @@
 # include <map>
 # include <cstdlib>
 # include <climits>
+# include <vector>
 
+class PmergeMe
+{
+    private :
+        int _odd;
+        std::vector<int> _vec;
+    public :
+        PmergeMe(const char **input) {}
+        PmergeMe(const PmergeMe& other) : _vec(other._vec) {}
+        PmergeMe& operator=(const PmergeMe& other){
+            if (this != &other) {
+                _vec = other._vec;
+                _odd = other._odd;
+            }
+            return *this;
+        }
+        ~PmergeMe() {}
+
+        void convert_vec(const char **input);
+        void sort();
+        void oddChecker();
+
+};
 
 #endif
